f92c9567f152eb27d8ff8809cf5f105454f9cfde
AnnRudneva <119349516+AnnRudneva@users.noreply.github.com>
2023-06-16 15:36:09 +0300

    Add files via upload

---
+++ PaymentsExampleApp/PaymentsExampleApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PaymentsExampleApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PaymentsBaseEntities _context = new PaymentsBaseEntities();    
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChartPayments.ChartAreas.Add(new ChartArea("Main"));
+            var currentSeries = new Series("Payments")
+            {
+                IsValueShownAsLabel = true
+            };
+            ChartPayments.Series.Add(currentSeries);
+            ComboUsers.ItemsSource = _context.User.ToList();
+            ComboChartTypes.ItemsSource = Enum.GetValues(typeof(SeriesChartType));
+        }
+    }
+}
